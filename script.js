d2f2fc5dfc94e3cb32fa4fe1b0a6b0c2c30b7977
Mentalism10 <mentalism45@gmail.com>
2026-06-13 23:48:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,20 +18,79 @@ canvas.addEventListener("mouseup", () => {
 canvas.addEventListener("mousemove", draw);
 
 function draw(e) {
-
     if (!drawing) return;
+
+    const x = e.clientX;
+    const y = e.clientY;
 
     ctx.lineWidth = 5;
     ctx.lineCap = "round";
-    ctx.strokeStyle = "black";
 
-    ctx.lineTo(e.clientX, e.clientY);
+    ctx.lineTo(x, y);
     ctx.stroke();
 
+    pushPoint(x, y);
     ctx.beginPath();
-    ctx.moveTo(e.clientX, e.clientY);
+    ctx.moveTo(x, y);
 }
 
-document.getElementById("clearBtn").onclick = () => {
+function clearBoard() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-};
+
+    fetch(
+        "https://guess-the-imposter-8bac0-default-rtdb.asia-southeast1.firebasedatabase.app/board.json",
+        {
+            method: "DELETE"
+        }
+    );
+}
+
+async function pushPoint(x, y) {
+    await fetch(
+        "https://guess-the-imposter-8bac0-default-rtdb.asia-southeast1.firebasedatabase.app/board.json",
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                x: x,
+                y: y
+            })
+        }
+    );
+}
+
+let loaded = {};
+
+async function syncBoard() {
+    const res = await fetch(
+        "https://guess-the-imposter-8bac0-default-rtdb.asia-southeast1.firebasedatabase.app/board.json"
+    );
+
+    const data = await res.json();
+
+    if (!data) return;
+
+    Object.keys(data).forEach(key => {
+
+        if (loaded[key]) return;
+
+        loaded[key] = true;
+
+        const point = data[key];
+
+        ctx.fillRect(
+            point.x,
+            point.y,
+            4,
+            4
+        );
+    });
+}
+
+setInterval(syncBoard, 500);
+
+document
+.getElementById("clear")
+.addEventListener("click", clearBoard);
