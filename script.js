9a0d52994fb05cb24d20157b673b248fc443bfc1
Mentalism10 <mentalism45@gmail.com>
2026-06-13 23:27:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
+const canvas = document.getElementById("board");
+const ctx = canvas.getContext("2d");
 
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let drawing = false;
+
+canvas.addEventListener("mousedown", () => {
+    drawing = true;
+});
+
+canvas.addEventListener("mouseup", () => {
+    drawing = false;
+    ctx.beginPath();
+});
+
+canvas.addEventListener("mousemove", draw);
+
+function draw(e) {
+
+    if (!drawing) return;
+
+    ctx.lineWidth = 5;
+    ctx.lineCap = "round";
+    ctx.strokeStyle = "black";
+
+    ctx.lineTo(e.clientX, e.clientY);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(e.clientX, e.clientY);
+}
+
+document.getElementById("clearBtn").onclick = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+};
